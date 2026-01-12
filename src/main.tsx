fbcd1a38e5de67ef2f8846451945597299e355d9
SamadRahmKhan <samadrahim47@gmail.com>
2026-01-12 10:33:33 +0500

    Fix GitHub Pages routing with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,13 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+// import { createRoot } from "react-dom/client";
+// import { HashRouter } from "react-router-dom";
+// import App from "./App.tsx";
+// import "./index.css";
+
+// createRoot(document.getElementById("root")!).render(
+//   <HashRouter>
+//     <App />
+//   </HashRouter>
+// );
