ea26d44549b00faf4c186c2bb1da852e5c5707f7
SamadRahmKhan <samadrahim47@gmail.com>
2026-01-12 22:16:10 +0500

    Fix Vite alias and base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,34 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://samad-rahim-portfolio.vercel.app",
+//         changeOrigin: true,
+//         secure: true,
+//       },
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/Samad-Rahim-Portfolio/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://samad-rahim-portfolio.vercel.app",
-        changeOrigin: true,
-        secure: true,
-      },
     },
   },
 });
