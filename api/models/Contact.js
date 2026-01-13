f0cb5c1c9e5c7171ff93d284548c939169772099
Samad Rahim <samadrahim47@gmail.com>
2026-01-13 09:55:29 +0500

    Create Contact model with Mongoose schema

---
+++ api/models/Contact.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const ContactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  subject: String,
+  message: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Contact ||
+  mongoose.model("Contact", ContactSchema);
